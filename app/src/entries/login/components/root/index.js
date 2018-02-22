1c9bc8aca1e2bcaeea72875d57402d37d171f79a
杨叙 <yx53074@hotmail.com>
2018-02-22 14:55:54 +0800

    111

--- app/src/entries/login/components/root/index.js
+++ app/src/entries/login/components/root/index.js
@@ -3,7 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import { Modal, Button } from "antd";
 import { I18n } from "react-i18next";
 import "./index.less";
-
+import loginHeader from "../../../../assets/images/login.png";
 export default class Root extends PureComponent {
 	constructor(props) {
 		super(props);
@@ -13,16 +13,15 @@ export default class Root extends PureComponent {
 	render() {
 		let { lng } = this.props;
 		return (
-			<div>22</div>
-			// <I18n>
-			// 	{(t, { i18n }) => (
-			// 		<div className="login">
-			// 			<div className="login-content">
-			// 				{t("login.login", lng)}
-			// 			</div>
-			// 		</div>
-			// 	)}
-			// </I18n>
+			<I18n>
+				{(t, { i18n }) => (
+					<div className="login">
+						<div className="login-content">
+							<div className="login-header" />
+						</div>
+					</div>
+				)}
+			</I18n>
 		);
 	}
 }
