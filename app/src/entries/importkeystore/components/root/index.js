4b464cf5db3fa48965460398a128892bd8bccf21
杨叙 <yx53074@hotmail.com>
2018-02-23 11:37:06 +0800

    11

--- app/src/entries/importkeystore/components/root/index.js
+++ app/src/entries/importkeystore/components/root/index.js
@@ -12,7 +12,34 @@ export default class Root extends PureComponent {
 		let { lng } = this.props;
 		return (
 			<I18n>
-				{(t, { i18n }) => <div className="importkeystore">22</div>}
+				{(t, { i18n }) => (
+					<div className="importkeystore">
+						<div className="keystore-title">Keystore</div>
+						<div className="keystore-container">
+							<div className="keystore-box">
+								<div className="keystore-group ui">
+									<i className="icon-check uncheck" />
+									<div className="f1">
+										<textarea className="keystore-text" />
+									</div>
+								</div>
+								<div className="keystore-group ui">
+									<i className="icon-check uncheck" />
+									<div className="f1 ui center">
+										<div className="f1">
+											<input
+												type="text"
+												disabled="disabled"
+												className="filename"
+											/>
+										</div>
+										<i className="icon-openfile" />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
 			</I18n>
 		);
 	}
